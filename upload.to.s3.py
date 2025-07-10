e957c1921649755ce7fe9ed96072fb798eab5f3f
aguero-tech <developer@aguero.tech>
2025-07-10 08:37:45 -0500

    link fix - return to main index page

--- upload.to.s3.py
+++ upload.to.s3.py
@@ -1,9 +1,13 @@
 import boto3
 from datetime import datetime
 import os
+from dotenv import load_dotenv
 
-BUCKET_NAME = 'rotaryradar-gurnee.aguero.tech'  # Updated bucket name
+load_dotenv()
+
+BUCKET_NAME = 'rotaryradar-gurnee.aguero.tech'
 EXPORT_DIR = 'exports'
+CLOUDFRONT_DISTRIBUTION_ID = os.getenv('CLOUDFRONT_DISTRIBUTION_ID')
 
 def upload_to_s3():
     today_str = datetime.today().strftime('%Y%m%d')
@@ -29,6 +33,21 @@ def upload_to_s3():
         ExtraArgs={'ContentType': 'text/html'}
     )
     print(f'Uploaded {index_file} to https://{BUCKET_NAME}.s3.amazonaws.com/index.html')
+
+    # Invalidate CloudFront cache for index.html
+    if CLOUDFRONT_DISTRIBUTION_ID:
+        cf = boto3.client('cloudfront')
+        invalidation = cf.create_invalidation(
+            DistributionId=CLOUDFRONT_DISTRIBUTION_ID,
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': 1,
+                    'Items': ['/index.html']
+                },
+                'CallerReference': str(datetime.utcnow().timestamp())
+            }
+        )
+        print(f"CloudFront invalidation created for /index.html: {invalidation['Invalidation']['Id']}")
 
 if __name__ == "__main__":
     upload_to_s3()
